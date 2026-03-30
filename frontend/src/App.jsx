5132b8a805941c711e1233d3dcfb7753905c19f1
Danil <danilguidjou7@gmail.com>
2026-03-30 17:10:49 +0200

    init page front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
-import AgentPage from './pages/AgentPage'
+import AgentPage from './pages/CreatAgentPage'
+
 
 export default function App() {
   return (
