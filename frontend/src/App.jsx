a1c349bfc7450514acc52fab159b4be9763173b1
Danil <danilguidjou7@gmail.com>
2026-03-30 15:29:54 +0200

    Page creation d'agent spécialiste

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import AgentPage from './pages/AgentPage'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      {/* Les autres pages (Login, Register, etc.) seront ajoutées ici */}
+      <Route path="/agents" element={<AgentPage />} />
     </Routes>
   )
 }
